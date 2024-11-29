11f7827add515d57bae3de4598883ffb9b5da967
JuniorDev <breinerbarraza22@gmail.com>
2024-11-29 16:13:45 -0500

    nuevos cambios

--- src/modules/security/services/useCases/login/index.ts
+++ src/modules/security/services/useCases/login/index.ts
@@ -1,7 +1,8 @@
 /* istanbul ignore file */
-import { LoginUser } from '@app/modules/security/services/useCases/login/loginUser.service';
+import { LoginUser } from './loginUser.service';
+import { RefreshToken } from './refreshToken.service';
 
 /**
  * Array of loginAttempt use cases.
  */
-export const LOGIN_USE_CASES = [LoginUser];
+export const LOGIN_USE_CASES = [LoginUser, RefreshToken];
